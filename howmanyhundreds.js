0791292f21a5939bc7344c7aed010956a98589a9
Nikko Bansil <bjohnnikko@gmail.com>
2022-10-23 14:55:53 -0700

    a function to check how many hundreds are there in a number

---
+++ howmanyhundreds.js
@@ -0,0 +1,11 @@
+function howManyHundreds(num){
+  let hundreds = 0;
+  hundreds = num / 100;
+  return Math.floor(hundreds);
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
