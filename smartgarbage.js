cc74db6e733f6d9ad2c088e0137b265d1cae4bbe
Nikko Bansil <bjohnnikko@gmail.com>
2022-10-25 21:17:14 -0700

    a function that takes a string and an object as the argument

---
+++ smartgarbage.js
@@ -0,0 +1,17 @@
+const smartGarbage = function (trash, bins) {
+  if (trash === 'waste') {
+    bins.waste += 1;
+  } else if (trash === 'recycling') {
+    bins.recycling += 1;
+  } else if (trash === 'compost') {
+    bins.compost += 1;
+  }
+  return bins;
+};
+
+let bins = {
+  waste: 0,
+  recycling: 0,
+  compost: 0
+};
+smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 });
